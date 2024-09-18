05b1127a960e98ff56bba3b9a688356d87cd04d9
Duc Duy Nghiem <ducduynghiem97@gmail.com>
2024-09-18 21:39:02 +0200

    Added models moniroting for admin

--- camera/admin.py
+++ camera/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CameraInfo, CameraService
 
-# Register your models here.
+@admin.register(CameraInfo)
+class CameraInfoAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'uuid', 'model', 'action_list_url')
+
+@admin.register(CameraService)
+class CameraServiceAdmin(admin.ModelAdmin):
+    list_display = ('camera_info', 'service_types')

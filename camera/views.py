5ade4f40d08e52e1a492a7195c9aab8671f9e2c1
Duc Duy Nghiem <ducduynghiem97@gmail.com>
2024-10-06 16:04:02 +0200

    Adjusted camera search view to communicate with the local serivce

--- camera/views.py
+++ camera/views.py
@@ -19,76 +19,6 @@ class CameraControlView(View):
 
 
 class FetchDeviceDescriptionView(View):
-    def _retrieve_location_url(self) -> tuple[Optional[str], Optional[str]]:
-        """
-        Get location URL using SSDP M-Search.
-        """
-        # SSDP M-SEARCH request to discover the camera
-        M_SEARCH = (
-            b"M-SEARCH * HTTP/1.1\r\n"
-            b"HOST: 239.255.255.250:1900\r\n"
-            b'MAN: "ssdp:discover"\r\n'
-            b"MX: 1\r\n"
-            b"ST: urn:schemas-sony-com:service:ScalarWebAPI:1\r\n"
-            b"USER-AGENT: Django/5.0 Python/3.x\r\n\r\n"
-        )
-
-        # Create socket for sending and receiving UDP packets over IPv4, necessary for SSDP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
-        # Allow socket to reuse the address and set socket options
-        sock.setsockopt(
-            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
-        )  # Allows multiple sockets to bind to the same address and port -> in case OS hasnt closed the socket zb. from previous run
-        sock.setsockopt(
-            socket.SOL_SOCKET, socket.SO_BROADCAST, 1
-        )  # Extend to Broadcast
-        sock.setsockopt(
-            socket.SOL_SOCKET, socket.SO_RCVBUF, 4096
-        )  # Incease buffer size
-
-        # Best to bind device IP directly ensures SSDP sends request through correct inteface
-        local_ip = "192.168.122.177"
-        sock.bind((local_ip, 0))
-
-        # Send M-SEARCH requests multiple times to increase probability that camera reponses
-        for i in range(5):
-            sock.sendto(M_SEARCH, ("239.255.255.250", 1900))
-            time.sleep(1)
-
-        location_url = None
-        start_time = time.time()
-        timeout = 8  # Total timeout in seconds
-        # Set a dynamic socket timeout to ensure that waiting for a response only takes exactly that long
-        try:
-            while True:
-                # Calculate remaining time
-                elapsed = time.time() - start_time
-                remaining = timeout - elapsed
-                if remaining <= 0:
-                    break
-                sock.settimeout(remaining)
-                try:
-                    data, addr = sock.recvfrom(1024)
-                    response_str = data.decode("utf-8")
-                    # Parse the response to get the LOCATION URL
-                    for line in response_str.splitlines():
-                        if line.startswith("LOCATION"):
-                            location_url = line.split(" ", 1)[1]
-                            break
-                    if location_url:
-                        break  # Exit loop if LOCATION URL is found
-                except socket.timeout:
-                    continue  # No data received, continue waiting
-        except Exception as e:
-            return None, f"Error: Failed to fetch location URL.\n{str(e)}"
-        finally:
-            sock.close()
-
-        if not location_url:
-            return None, "Error: Location URL not found in SSDP response."
-        return location_url, None
-
     def _get_namespace(self, element) -> str:
         """
         Extracts the namespace from an XML element.
@@ -174,53 +104,47 @@ class FetchDeviceDescriptionView(View):
         """
         Use location URL to fetch the device description, parse it, and save to the database.
         """
-        location_url, error = self._retrieve_location_url()
-        if error:
-            logging.error(error)
-            return render(
-                request,
-                "camera_control.html",
-                {"alert": "Fetching location URL failed."},
-            )
-
         try:
-            response = requests.get(location_url)
+            response = requests.get("http://localhost:8001/discover")
             if response.status_code == 200:
-                xml_content = response.content
-
-                # Parse the XML content
-                camera_data, parse_error = self._parse_device_description(xml_content)
-                if parse_error:
-                    logging.error(parse_error)
-                    return render(
-                        request,
-                        "camera_control.html",
-                        {"alert": "Parsing device discription failed."},
-                    )
-
-                # Save the parsed data into the models
-                success, save_error = self._save_camera_info(camera_data)
-                if save_error:
-                    return render(request, "camera_control.html", {"alert": save_error})
-
-                return render(
-                    request,
-                    "camera_control.html",
-                    {"alert": "Device description retrieved and saved successfully!"},
-                )
+                data = response.json()
+                error_alert = ""
+                if "error" in data:
+                    error_alert = data["error"]
+                else:
+                    device_description = data.get("device_description")
+                    print(device_description)
             else:
+                error_alert = "Failed to fetch device description from local service."
+        except Exception as e:
+            error_alert = f"Error: {str(e)}"
+
+        if not error_alert:
+            camera_data, parse_error = self._parse_device_description(
+                device_description
+            )
+            if parse_error:
+                logging.error(parse_error)
                 return render(
                     request,
                     "camera_control.html",
-                    {"alert": "Error: Failed to fetch device description."},
+                    {"alert": "Parsing device discription failed."},
                 )
+            success, save_error = self._save_camera_info(camera_data)
+            if save_error:
+                return render(request, "camera_control.html", {"alert": save_error})
 
-        except Exception as e:
-            logging.error(f"Error: {str(e)}")
             return render(
                 request,
                 "camera_control.html",
-                {"alert": f"Error: Failed to fetch device description."},
+                {"alert": "Device description retrieved and saved successfully!"},
+            )
+        else:
+            logging.error(error_alert)
+            return render(
+                request,
+                "camera_control.html",
+                {"alert": "Error at retrieving device descriptions."},
             )
 
 

d3101bfda98b0d5d51c906725911de8ba18c7d23
Duc Duy Nghiem <ducduynghiem97@gmail.com>
2025-01-27 18:25:08 +0100

    removed redundant packages

--- sony_camera_control/settings.py
+++ sony_camera_control/settings.py
@@ -38,13 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "camera",
-    "crispy_forms",
-    "crispy_bootstrap5",
-    "django_sass",
 ]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
